96e69f8147b39beee1ad0ce3c9a32aa358d37eb6
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-12-05 16:58:28 -0600

    Create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 
 import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -13,12 +18,19 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
-      return await newUser.save();
+      const { password, ...userData } = createUserDto;
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
     } catch (error) {
       if (error.code === 11000) {
         throw new BadRequestException(`${createUserDto.email} already exists.`);
       }
+      throw new InternalServerErrorException('Somthing terribe happen.');
     }
   }
 
